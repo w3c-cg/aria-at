7a24d70ce1db9b5d7046dc09ea2332c90142fc66
lola odelola <lolaodelola@gmail.com>
2026-08-13 15:25:16 +0100

    Revert "button tests"
    This reverts commit 6d91cf4f3e885575b2acf14eac97f107bd1f4ede.

--- tests/html-aam/button/data/js/setFocusAfterButton.js
+++
@@ -1,2 +0,0 @@
-// sets focus on a link after the button
-testPageDocument.querySelector('#afterlink').focus();
--- tests/html-aam/button/data/js/setFocusBeforeDisabledButton.js
+++
@@ -1,2 +0,0 @@
-// sets focus on a link before the disabled button
-testPageDocument.querySelector('#beforelink-disabled').focus();
