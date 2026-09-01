c998ae93e37a83bee3b16429adafdce532d791e5
lola odelola <lolaodelola@gmail.com>
2026-09-01 17:39:50 +0100

    rm disable button test plan

--- tests/html-aam/disabled-button/data/js/setFocusAfterDisabledButton.js
+++
@@ -1,2 +0,0 @@
-// sets focus on a link after the disabled button
-testPageDocument.querySelector('#afterlink-disabled').focus();
--- tests/html-aam/disabled-button/data/js/setFocusBeforeButton.js
+++
@@ -1,2 +0,0 @@
-// sets focus on a link before the button
-testPageDocument.querySelector('#beforelink').focus();
